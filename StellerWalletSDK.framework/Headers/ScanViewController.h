b4501ad5fe87d915a062b3b352054adddac3b114
贾利鑫 <1101461014@qq.com>
2019-12-16 16:32:37 +0800

    init

---
+++ StellerWalletSDK.framework/Headers/ScanViewController.h
@@ -0,0 +1,17 @@
+//
+//  ScanViewController.h
+//  Stellar
+//
+//  Created by 贾利鑫 on 2019/12/10.
+//  Copyright © 2019 贾利鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@class  payMessageVC;
+@interface ScanViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StellerWalletSDK.framework/Headers/StellerWalletSDK.h
@@ -0,0 +1,28 @@
+//
+//  StellerWalletSDK.h
+//  StellerWalletSDK
+//
+//  Created by 贾利鑫 on 2019/12/16.
+//  Copyright © 2019 贾利鑫. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StellerWalletSDK.
+FOUNDATION_EXPORT double StellerWalletSDKVersionNumber;
+
+//! Project version string for StellerWalletSDK.
+FOUNDATION_EXPORT const unsigned char StellerWalletSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StellerWalletSDK/PublicHeader.h>
+
+//#import "walletViewController.h"
+//#import "Jcommen.h"
+#import <StellerWalletSDK/ScanViewController.h>
+#import <StellerWalletSDK/WSLNativeScanTool.h>
+#import <StellerWalletSDK/WSLScanView.h>
+//#import "ScanViewController.h"
+
+
+
+
